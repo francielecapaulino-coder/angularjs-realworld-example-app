fd2d821c23b8d797a245dc85539a29c3ec4f3260
Franciele Paulino <franciele.paulino@globant.com>
2026-06-26 09:56:48 -0300

    feat: bootstrap Spring Boot + Maven skeleton for conduit backend

---
+++ src/main/java/com/conduit/ConduitBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.conduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConduitBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConduitBackendApiApplication.class, args);
+	}
+
+}
